41d9080a0d534d154ad781a01088dcc2e9a1339f
urvaius <urvaius@gmail.com>
2017-12-01 15:37:59 -0800

    just trying some things nothing really.

---
+++ SaturnsTurn4DesktopGL/Utility/AddEntity.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaturnsTurn4DesktopGL.Utility
+{
+    public class AddEntity
+    {
+        Animation animation;
+        Vector2 position;
+        Texture2D texture;
+
+        public void AddToGame(Animation animation, Vector2 position, Texture2D texture)
+        {
+            this.animation = animation;
+            this.position = position;
+            this.texture = texture;
+
+            animation = new Animation();
+
+        }
+    }
+}
